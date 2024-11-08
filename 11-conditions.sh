0b84d0319ff8fd8a599e071e30b97e58bad15368
Sekhar <sekhar2216@gmail.com>
2024-11-08 13:59:15 +0530

    shell

---
+++ 11-conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Number=$1
+
+if [ $Number  -gt 20 ]
+then 
+    echo "Given number: $Number is greater than 20"
+else
+    echo "Given number: $Number is less than 20"
+fi
+
+
+
+#gt -- greater than
+#lt -- less than
+#ne -- not equal
+#ge --  greater than equal
+#le -- less than equal
