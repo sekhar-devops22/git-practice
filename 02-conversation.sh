9b2de3dbafe961967e1bd9b0255fd2bb89e6b14f
Sekhar <sekhar2216@gmail.com>
2024-11-08 11:08:19 +0530

    shell

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "sekhar:: Hi siri, how are you?"
+echo "siri:: hello sekhar. I am fine. how are you doing?"
+echo "sekhar:: I am doing good siri. what's going on?"
+echo "siri:: I started learning shell script"
