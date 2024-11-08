eb270308ed986374ad2ca65204ce360f208a79d6
Sekhar <sekhar2216@gmail.com>
2024-11-08 12:07:06 +0530

    shell

---
+++ 07-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# index starts from 0
+
+Fruits=("Apple", "Kiwi","grapes","orange") #Array
+
+echo "first fruit is: ${fruits[0]}"
+echo "second fruit is: ${fruit[1]}"
+echo "third fruit is: ${fruit[2]}"
+echo "fourth fruit is: ${fruit[3]}"
+
+
+echo "all fruits is: ${fruit[@]}"
